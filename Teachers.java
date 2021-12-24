0e4a9e4ff24a9d5cd505b17fb3f23698de769ada
Md. Motaleb Hossain <95126535+motaleb1987@users.noreply.github.com>
2021-12-24 18:09:58 +0600

    Add files via upload

--- Teachers.java
+++ Teachers.java
@@ -3,4 +3,17 @@ package com.company;
 public class Teachers {
     String name, gender;
     int mobile;
+
+    void setInformation(String n,String g,int mob){
+        name=n;
+        gender=g;
+        mobile=mob;
+    }
+
+    void displayInformation(){
+        System.out.println("Name :"+name);
+        System.out.println("Gender :"+gender);
+        System.out.println("Mobile :"+mobile);
+        System.out.println();
+    }
 }
